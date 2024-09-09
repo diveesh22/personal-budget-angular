c61e7ace24af0b6b4423f46d5d20556b448743e0
Diveesh Sai Udayagiri <diveeshsaiudayagiri@Diveeshs-Air.lan>
2024-09-08 23:23:38 -0400

    directory

--- server.js
+++ server.js
@@ -1,34 +1,18 @@
-// Budget API
-
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 3000;
+const budgetData = require('./budget-data.json');
 
-app.use(cors());
+app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 25
-        },
-        {
-            title: 'Rent',
-            budget: 275
-        },
-        {
-            title: 'Grocery',
-            budget: 110
-        },
-    ]
-};
-
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(budgetData);
 });
 
 app.listen(port, () => {
-    console.log(`API served at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
